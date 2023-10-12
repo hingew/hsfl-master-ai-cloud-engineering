f768700c4de899fde9de161e6ddb7f0ad63aa9a2
Hauke Ingwersen <hauke.ingwersen@stud.hs-flensburg.de>
2023-10-12 19:53:35 +0200

    fix(user-service): Fix go.mod and main.go

--- user-service/go.mod
+++ user-service/go.mod
@@ -3,14 +3,11 @@ module github.com/hingew/hsfl-master-ai-cloud-engineering/user-service
 go 1.21.1
 
 require (
-	github.com/hingew/hsfl-master-ai-cloud-engineering/lib v0.0.0-00010101000000-000000000000
+	github.com/hingew/hsfl-master-ai-cloud-engineering/lib v0.0.0
 	github.com/lib/pq v1.10.9
-)
-
-require (
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	golang.org/x/crypto v0.14.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
 
-replace github.com/hingew/hsfl-master-ai-cloud-engineering/lib => ../lib
+replace github.com/hingew/hsfl-master-ai-cloud-engineering/lib v0.0.0 => ../lib/
--- user-service/main.go
+++ user-service/main.go
@@ -1,51 +1,33 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"log"
 	"net/http"
-	"os"
 
+	"github.com/hingew/hsfl-master-ai-cloud-engineering/lib/config"
 	"github.com/hingew/hsfl-master-ai-cloud-engineering/lib/database"
 	"github.com/hingew/hsfl-master-ai-cloud-engineering/user-service/api/handler"
 	"github.com/hingew/hsfl-master-ai-cloud-engineering/user-service/api/router"
 	"github.com/hingew/hsfl-master-ai-cloud-engineering/user-service/auth"
 	"github.com/hingew/hsfl-master-ai-cloud-engineering/user-service/crypto"
 	"github.com/hingew/hsfl-master-ai-cloud-engineering/user-service/user"
-	"gopkg.in/yaml.v3"
 )
 
 type ApplicationConfig struct {
 	Database database.PsqlConfig `yaml:"database"`
 	Jwt      auth.JwtConfig      `yaml:"jwt"`
-}
-
-func LoadConfigFromFile(path string) (*ApplicationConfig, error) {
-	f, err := os.Open(path)
-	if err != nil {
-		return nil, err
-	}
-
-	var config ApplicationConfig
-	if err := yaml.NewDecoder(f).Decode(&config); err != nil {
-		return nil, err
-	}
-
-	return &config, nil
+	Port     int                 `yaml:"port"`
 }
 
 func main() {
-	port := flag.String("port", "8080", "The listening port")
-	configPath := flag.String("config", "config.yml", "The path to the configuration file")
-	flag.Parse()
+	var appConfig *ApplicationConfig
 
-	config, err := LoadConfigFromFile(*configPath)
-	if err != nil {
+	if err := config.Load(appConfig); err != nil {
 		log.Fatalf("could not load application configuration: %s", err.Error())
 	}
 
-	userRepository, err := user.NewPsqlRepository(config.Database)
+	userRepository, err := user.NewPsqlRepository(appConfig.Database)
 	if err != nil {
 		log.Fatalf("could not create user repository: %s", err.Error())
 	}
@@ -54,7 +36,7 @@ func main() {
 		log.Fatalf("could not migrate: %s", err.Error())
 	}
 
-	tokenGenerator, err := auth.NewJwtTokenGenerator(config.Jwt)
+	tokenGenerator, err := auth.NewJwtTokenGenerator(appConfig.Jwt)
 	if err != nil {
 		log.Fatalf("could not create JWT token generator: %s", err.Error())
 	}
@@ -66,7 +48,7 @@ func main() {
 		handler.NewLoginHandler(userRepository, hasher, tokenGenerator),
 	)
 
-	addr := fmt.Sprintf("0.0.0.0:%s", *port)
+	addr := fmt.Sprintf("0.0.0.0:%d", *&appConfig.Port)
 	if err := http.ListenAndServe(addr, handler); err != nil {
 		log.Fatalf("error while listen and serve: %s", err.Error())
 	}
