7980270bb2a4f3f036f8db1b40dc594e2b8f0e75
Robert Pfeiffer <robert.pfeiffer@stud.hs-coburg.de>
2023-10-18 22:51:56 +0200

    test: added unit test to user-service

---
+++ lib/containerhelpers/postgres.go
@@ -0,0 +1,26 @@
+package containerhelpers
+
+import (
+	"context"
+
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/wait"
+)
+
+func StartPostgres() (testcontainers.Container, error) {
+	req := testcontainers.ContainerRequest{
+		Image:        "postgres:latest",
+		ExposedPorts: []string{"5432/tcp"},
+		Env: map[string]string{
+			"POSTGRES_USER":     "postgres",
+			"POSTGRES_PASSWORD": "postgres",
+			"POSTGRES_DB":       "postgres",
+		},
+		WaitingFor: wait.ForListeningPort("5432/tcp"),
+	}
+
+	return testcontainers.GenericContainer(context.Background(), testcontainers.GenericContainerRequest{
+		ContainerRequest: req,
+		Started:          true,
+	})
+}
---
+++ services/user-service/api/router/router_test.go
@@ -0,0 +1,67 @@
+package router
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	mocks "github.com/hingew/hsfl-master-ai-cloud-engineering/user-service/_mocks"
+	"github.com/stretchr/testify/assert"
+	"go.uber.org/mock/gomock"
+)
+
+func TestRouter(t *testing.T) {
+	ctrl := gomock.NewController(t)
+
+	registerHandler := mocks.NewMockHandler(ctrl)
+	loginHandler := mocks.NewMockHandler(ctrl)
+	router := New(registerHandler, loginHandler)
+
+	t.Run("should run register handler", func(t *testing.T) {
+		// given
+		w := httptest.NewRecorder()
+		r := httptest.NewRequest("POST", "/api/v1/auth/register", nil)
+
+		registerHandler.
+			EXPECT().
+			ServeHTTP(w, r).
+			Times(1)
+
+		// when
+		router.ServeHTTP(w, r)
+
+		// then
+		assert.Equal(t, http.StatusOK, w.Code)
+		assert.True(t, ctrl.Satisfied())
+	})
+
+	t.Run("should run login handler", func(t *testing.T) {
+		// given
+		w := httptest.NewRecorder()
+		r := httptest.NewRequest("POST", "/api/v1/auth/login", nil)
+
+		loginHandler.
+			EXPECT().
+			ServeHTTP(w, r).
+			Times(1)
+
+		// when
+		router.ServeHTTP(w, r)
+
+		// then
+		assert.Equal(t, http.StatusOK, w.Code)
+		assert.True(t, ctrl.Satisfied())
+	})
+
+	t.Run("should return 404 NOT FOUND if target is unknown", func(t *testing.T) {
+		// given
+		w := httptest.NewRecorder()
+		r := httptest.NewRequest("POST", "/api/v1/auth/unknown", nil)
+
+		// when
+		router.ServeHTTP(w, r)
+
+		// then
+		assert.Equal(t, http.StatusNotFound, w.Code)
+	})
+}
