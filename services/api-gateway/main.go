197411631612f19f770cbc656a630ae203737adc
YOGA-7\Jannes <jannes.nebendahl@gmx.de>
2023-11-05 20:37:26 +0100

    push for hauke

--- services/api-gateway/main.go
+++ services/api-gateway/main.go
@@ -3,13 +3,14 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	my_proxy "github.com/hingew/hsfl-master-ai-cloud-engineering/api-gateway/proxy"
 )
 
 func main() {
 	// templateing_service_endpoint := os.Getenv("TEMPLATE_ENDPOINT")
-	// web_service_endpoint := os.Getenv("WEB_ENDPOINT")
+	web_service_endpoint := os.Getenv("WEB_ENDPOINT")
 	// creation_service_endpoint := os.Getenv("CREATION_ENDPOINT")
 	// user_service_endpoint := os.Getenv("USER_ENDPOINT")
 
@@ -18,7 +19,7 @@ func main() {
 		log.Fatalf("could not create reverse proxy: %s", err.Error())
 	}
 
-	proxy.Map("/", "http://localhost:3000/")
+	proxy.Map("/", web_service_endpoint)
 
 	if err := http.ListenAndServe(":3000", proxy); err != nil {
 		log.Fatalf("error while listen and serve: %s", err.Error())
