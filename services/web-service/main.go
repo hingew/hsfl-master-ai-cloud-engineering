e1dc35ea30be22ee84dfa6cc9b1349ed83433ab0
Robert Pfeiffer <robert.pfeiffer@stud.hs-coburg.de>
2023-11-09 15:04:58 +0100

    feature: add css and use time data type

--- services/web-service/main.go
+++ services/web-service/main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 )
 
 type IndexPageViewModel struct {
@@ -14,8 +15,8 @@ type IndexPageViewModel struct {
 }
 
 type PdfTemplate struct {
-	UpdatedAt string
-	CreatedAt string
+	UpdatedAt time.Time
+	CreatedAt time.Time
 	Name      string
 }
 
