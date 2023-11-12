60af5312a4e2b1b5a5cd0b29226a7653d39d5383
YOGA-7\Jannes <jannes.nebendahl@gmx.de>
2023-11-12 21:14:47 +0100

    make it runable without parameters

--- services/orchestator/main.go
+++ services/orchestator/main.go
@@ -1,19 +1,11 @@
 package main
 
 import (
-	"flag"
-
 	"github.com/hingew/hsfl-master-ai-cloud-engineering/lib/orchestrator"
 )
 
 func main() {
-	image := flag.String("image", "", "")
-	replicas := flag.Int("replicas", 1, "")
-	network := flag.String("network", "bridge", "")
-	port := flag.Int("port", 3000, "")
-	flag.Parse()
-
-	orchestrator, err := orchestrator.NewOrchestrator(*image, *replicas, *network, *port)
+	orchestrator, err := orchestrator.NewOrchestrator("kluddizz/simple-server:latest", 6, "bridge", 3000)
 	if err != nil {
 		panic(err)
 	}
