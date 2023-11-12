ead611743b771228080e6bc6bf5919d954f1364f
YOGA-7\Jannes <jannes.nebendahl@gmx.de>
2023-11-12 21:02:02 +0100

    capsulated orchestrator

---
+++ lib/orchestrator/orchestrator.go
@@ -0,0 +1,130 @@
+package orchestrator
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/client"
+	loadbalancer "github.com/hingew/hsfl-master-ai-cloud-engineering/lib/load_balancer"
+)
+
+type Orchestrator struct {
+	image    string
+	replicas int
+	network  string
+	port     int
+	client   *client.Client
+}
+
+func NewOrchestrator(image string, replicas int, network string, port int) (*Orchestrator, error) {
+	cli, err := client.NewClientWithOpts()
+	if err != nil {
+		return nil, err
+	}
+
+	orc := Orchestrator{
+		image:    image,
+		replicas: replicas,
+		network:  network,
+		port:     port,
+		client:   cli,
+	}
+
+	return &orc, nil
+}
+
+func (orc *Orchestrator) Start() {
+	containerIds, err := orc.startContainers()
+	if err != nil {
+		panic(err)
+	}
+
+	containerEndpoints, err := orc.evaluateContainerEndpoints(*containerIds)
+	if err != nil {
+		panic(err)
+	}
+
+	defer orc.stopContainers(*containerIds)
+
+	balancer := loadbalancer.NewLoadBalancer(containerEndpoints, loadbalancer.RoundRobin)
+
+	addr := fmt.Sprintf(":%d", orc.port)
+	server := &http.Server{
+		Addr:    addr,
+		Handler: balancer,
+	}
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		<-sigChan
+		server.Shutdown(context.Background())
+
+	}()
+
+	server.ListenAndServe()
+}
+
+func (orc *Orchestrator) evaluateContainerEndpoints(containerIds []string) ([]*url.URL, error) {
+	endpoints := make([]*url.URL, len(containerIds))
+
+	for i, id := range containerIds {
+		container, err := orc.client.ContainerInspect(context.Background(), id)
+		if err != nil {
+			return nil, err
+		}
+
+		ip := container.NetworkSettings.Networks[orc.network].IPAddress
+
+		rawUrl := fmt.Sprintf("http://%s:%d", ip, orc.port)
+		endpoint, err := url.Parse(rawUrl)
+		if err != nil {
+			return nil, err
+		}
+
+		endpoints[i] = endpoint
+	}
+
+	return endpoints, nil
+}
+
+func (orc *Orchestrator) startContainers() (*[]string, error) {
+	pullResponse, err := orc.client.ImagePull(context.Background(), orc.image, types.ImagePullOptions{})
+	if err != nil {
+		return nil, err
+	}
+	defer pullResponse.Close()
+
+	io.Copy(os.Stdout, pullResponse)
+
+	containerIds := make([]string, orc.replicas)
+	for i := 0; i < orc.replicas; i++ {
+		createResponse, err := orc.client.ContainerCreate(context.Background(), &container.Config{Image: orc.image}, &container.HostConfig{}, nil, nil, "")
+		if err != nil {
+			return nil, err
+		}
+
+		if err := orc.client.ContainerStart(context.Background(), createResponse.ID, types.ContainerStartOptions{}); err != nil {
+			return nil, err
+		}
+
+		containerIds[i] = createResponse.ID
+	}
+	return &containerIds, err
+}
+
+func (orc *Orchestrator) stopContainers(containerIds []string) {
+	for _, id := range containerIds {
+		if err := orc.client.ContainerRemove(context.Background(), id, types.ContainerRemoveOptions{Force: true}); err != nil {
+			panic(err)
+		}
+	}
+}
--- services/orchestator/main.go
+++ services/orchestator/main.go
@@ -1,20 +1,9 @@
 package main
 
 import (
-	"context"
 	"flag"
-	"fmt"
-	"io"
-	"net/http"
-	"net/url"
-	"os"
-	"os/signal"
-	"syscall"
 
-	"github.com/docker/docker/api/types"
-	"github.com/docker/docker/api/types/container"
-	"github.com/docker/docker/client"
-	loadbalancer "github.com/hingew/hsfl-master-ai-cloud-engineering/lib/load_balancer"
+	"github.com/hingew/hsfl-master-ai-cloud-engineering/lib/orchestrator"
 )
 
 func main() {
@@ -24,97 +13,10 @@ func main() {
 	port := flag.Int("port", 3000, "")
 	flag.Parse()
 
-	containers := StartContainers(*image, *replicas)
-	endpoints := EvaluateContainerEndpoint(containers, *network, *port)
-	defer StopContainers(containers)
-
-	balancer := loadbalancer.NewLoadBalancer(endpoints, loadbalancer.RoundRobin)
-
-	addr := fmt.Sprintf(":%d", *port)
-	server := &http.Server{
-		Addr:    addr,
-		Handler: balancer,
-	}
-
-	sigChan := make(chan os.Signal, 1)
-	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
-	go func() {
-		<-sigChan
-		server.Shutdown(context.Background())
-
-	}()
-
-	server.ListenAndServe()
-}
-
-func EvaluateContainerEndpoint(containerIds []string, networkName string, port int) []*url.URL {
-	cli, err := client.NewClientWithOpts()
+	orchestrator, err := orchestrator.NewOrchestrator(*image, *replicas, *network, *port)
 	if err != nil {
 		panic(err)
 	}
 
-	endpoints := make([]*url.URL, len(containerIds))
-
-	for i, id := range containerIds {
-		container, err := cli.ContainerInspect(context.Background(), id)
-		if err != nil {
-			panic(err)
-		}
-
-		ip := container.NetworkSettings.Networks[networkName].IPAddress
-
-		rawUrl := fmt.Sprintf("http://%s:%d", ip, port)
-		endpoint, err := url.Parse(rawUrl)
-		if err != nil {
-			panic(err)
-		}
-
-		endpoints[i] = endpoint
-	}
-
-	return endpoints
-}
-
-func StopContainers(containers []string) {
-	cli, err := client.NewClientWithOpts()
-	if err != nil {
-		panic(err)
-	}
-
-	for _, container := range containers {
-		if err := cli.ContainerRemove(context.Background(), container, types.ContainerRemoveOptions{Force: true}); err != nil {
-			panic(err)
-		}
-	}
-
-}
-
-func StartContainers(image string, replicas int) []string {
-	cli, err := client.NewClientWithOpts()
-	if err != nil {
-		panic(err)
-	}
-
-	pullResponse, err := cli.ImagePull(context.Background(), image, types.ImagePullOptions{})
-	if err != nil {
-		panic(err)
-	}
-	defer pullResponse.Close()
-
-	io.Copy(os.Stdout, pullResponse)
-
-	containers := make([]string, replicas)
-	for i := 0; i < replicas; i++ {
-		createResponse, err := cli.ContainerCreate(context.Background(), &container.Config{Image: image}, &container.HostConfig{}, nil, nil, "")
-		if err != nil {
-			panic(err)
-		}
-
-		if err := cli.ContainerStart(context.Background(), createResponse.ID, types.ContainerStartOptions{}); err != nil {
-			panic(err)
-		}
-
-		containers[i] = createResponse.ID
-	}
-	return containers
+	orchestrator.Start()
 }
