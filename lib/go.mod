3c6510f260cf6b0c5a072fd472012148c0a1c3f6
Hauke Ingwersen <me@hing.dev>
2023-11-15 19:13:07 +0100

    feat(health-check): Add health endpoints to services

--- lib/go.mod
+++ lib/go.mod
@@ -6,7 +6,6 @@ require (
 	github.com/stretchr/testify v1.8.4
 	github.com/testcontainers/testcontainers-go v0.25.0
 	go.uber.org/mock v0.3.0
-	gopkg.in/yaml.v3 v3.0.1
 )
 
 require (
@@ -53,4 +52,5 @@ require (
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20230525234030-28d5490b6b19 // indirect
 	google.golang.org/grpc v1.57.0 // indirect
 	google.golang.org/protobuf v1.30.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- services/templateing-service/api/router/router.go
+++ services/templateing-service/api/router/router.go
@@ -1,30 +1,22 @@
 package router
 
 import (
-	"net/http"
-
+	"github.com/hingew/hsfl-master-ai-cloud-engineering/lib/health"
 	"github.com/hingew/hsfl-master-ai-cloud-engineering/lib/router"
 	"github.com/hingew/hsfl-master-ai-cloud-engineering/templateing-service/templates/controller"
 )
 
-type TemplateRouter struct {
-	router http.Handler
-}
-
 func NewTemplateRouter(
 	myController controller.Controller,
-) *TemplateRouter {
-	myRouter := router.New()
+) *router.Router {
+	templateRouter := router.New()
+    templateRouter.GET("/api/health/templates", health.Check)
 
-	myRouter.GET("/api/templates", myController.GetAllTemplates)
-	myRouter.GET("/api/templates/:id", myController.GetTemplate)
-	myRouter.POST("/api/templates", myController.CreateTemplate)
-	myRouter.PUT("/api/templates/:id", myController.UpdateTemplate)
-	myRouter.DELETE("/api/templates/:id", myController.DeleteTemplate)
+	templateRouter.GET("/api/templates", myController.GetAllTemplates)
+	templateRouter.GET("/api/templates/:id", myController.GetTemplate)
+	templateRouter.POST("/api/templates", myController.CreateTemplate)
+	templateRouter.PUT("/api/templates/:id", myController.UpdateTemplate)
+	templateRouter.DELETE("/api/templates/:id", myController.DeleteTemplate)
 
-	return &TemplateRouter{myRouter}
-}
-
-func (router *TemplateRouter) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	router.router.ServeHTTP(w, r)
+	return templateRouter
 }
