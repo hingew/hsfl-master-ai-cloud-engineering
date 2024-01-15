18987c1fd50faa604ff00e57725aaa03aae3f8d5
Hauke Ingwersen <me@hing.dev>
2024-01-15 20:35:38 +0100

    chore(api-gateway): Remove unused code, update .env.example

--- services/api-gateway/main.go
+++ services/api-gateway/main.go
@@ -4,7 +4,6 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"strings"
 
 	my_proxy "github.com/hingew/hsfl-master-ai-cloud-engineering/api-gateway/proxy"
 )
@@ -16,30 +15,6 @@ type RoutesConfig struct {
 	CreationService    []string
 }
 
-func readRoutesFromEnv() RoutesConfig {
-	var routes RoutesConfig
-
-	routes.CreationService = readRouteEnv("CREATION_ROUTES")
-	routes.WebService = readRouteEnv("WEB_ROUTES")
-	routes.TemplateingService = readRouteEnv("TEMPLATE_ROUTES")
-	routes.UserService = readRouteEnv("USER_ROUTES")
-
-	return routes
-}
-
-func readRouteEnv(env string) []string {
-	value := os.Getenv(env)
-	values := strings.Split(value, ";")
-	return values
-}
-
-//func addRoutes(proxy my_proxy.ReverseProxy, endpoint string, routes []string) {
-//	for _, source_route := range routes {
-//		target_route := fmt.Sprintf("%s%s", endpoint, source_route)
-//		proxy.Map(source_route, target_route)
-//	}
-//}
-
 func main() {
 	templateing_service_endpoint := os.Getenv("TEMPLATE_ENDPOINT")
 	web_service_endpoint := os.Getenv("WEB_ENDPOINT")
@@ -49,10 +24,10 @@ func main() {
 	proxy := my_proxy.NewHttpReverseProxy(http.DefaultClient)
 
 	// Add configurations to the proxy
-    proxy.Map("/auth/*", user_service_endpoint, false)
-    proxy.Map("/api/templates/*", templateing_service_endpoint, false)
+	proxy.Map("/auth/*", user_service_endpoint, false)
+	proxy.Map("/api/templates/*", templateing_service_endpoint, false)
 	proxy.Map("/api/render/*", creation_service_endpoint, false)
-    proxy.Map("/*", web_service_endpoint, false)
+	proxy.Map("/*", web_service_endpoint, false)
 
 	if err := http.ListenAndServe(":3000", proxy); err != nil {
 		log.Fatalf("error while listen and serve: %s", err.Error())
